72cc570237e25ad106415abb60c9a8eb9e8fa531
amberkar-harsh-02 <harshamberkar02@gmail.com>
2026-04-18 14:10:51 -0700

    working analysis

--- samples/CameraAccess/app/src/main/java/com/meta/wearable/dat/externalsampleapps/cameraaccess/stream/AnalysisConfig.kt
+++ samples/CameraAccess/app/src/main/java/com/meta/wearable/dat/externalsampleapps/cameraaccess/stream/AnalysisConfig.kt
@@ -2,7 +2,7 @@ package com.meta.wearable.dat.externalsampleapps.cameraaccess.stream
 
 internal object AnalysisConfig {
   // Example: http://192.168.1.50:8000/analyze-frame
-  const val endpointUrl = "ws://10.9.149.158:8000/analyze"
+  const val endpointUrl = "http://10.225.106.47:8000/analyze"
 
   // Send one frame every 500ms by default.
   const val frameIntervalMs = 500L
